ee2012de5e3085cce771cce084ce3695f9e67b33
Son Nguyen <son.nguyen@bytedance.com>
2024-08-08 04:13:26 +0000

    Update

--- update.sh
+++ update.sh
@@ -2,8 +2,25 @@
 
 set -e
 
+echo "========================================"
+echo "1. Current status"
 git status
+echo "========================================"
+echo
+
 git add -u
+
+echo "========================================"
+echo "2. Next status"
 git status
+echo "========================================"
+echo
+
 git commit -m "Update"
 git push
+
+echo "========================================"
+echo "3. Final status"
+git status
+echo "========================================"
+echo
