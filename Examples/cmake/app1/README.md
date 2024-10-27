424250b9b64a35616994f5e9523b76a694e808f2
Son Nguyen <son.nguyen@bytedance.com>
2024-10-27 03:57:47 +0000

    Add cmake app1

---
+++ Examples/cmake/app1/README.md
@@ -0,0 +1,7 @@
+# How to build
+```Bash
+rm -rf build && mkdir build
+cd build
+cmake ../src
+cmake --build .
+```
---
+++ Examples/cmake/app1/src/main.cc
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main(int argc, char** argv) {
+  std::cout << "Hello cmake!" << std::endl;
+  return 0;
+}
