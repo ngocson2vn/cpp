567e7fc405b52dbb03ad29e784018364d63d3050
Son Nguyen <son.nguyen@bytedance.com>
2026-02-28 04:01:01 +0000

    Update docker commands

--- docs/docker.sh
+++ docs/docker.sh
@@ -1,14 +1,31 @@
-# interactive mode
-docker run -it --name <container_name> <image_name> /usr/bin/bash
+#==================================
+# Docker daemon configs
+#==================================
+apt install -y nvidia-container-toolkit
+cat <<EOF > /etc/docker/daemon.json
+{
+    "runtimes": {
+        "nvidia": {
+            "args": [],
+            "path": "nvidia-container-runtime"
+        }
+    },
+    "data-root": "/data01/docker",
+    "ipv6": true,
+    "fixed-cidr-v6": "fd00::/80"
+}
+EOF
 
-# daemon mode
-docker run -d --name <container_name> <image_name> /usr/bin/bash -c 'while true; do sleep 3600; done'
+#==================================
+# Enable gpus and privileged mode
+#==================================
+IMAGE=image_name
+CONTAINER=container_name
+# Interactive mode for debugging
+docker run --rm -it --privileged --gpus all --network host --cgroupns host -v /sys/fs/cgroup:/sys/fs/cgroup:rw --name ${CONTAINER} $IMAGE /sbin/init
+# Deamon mode
+docker run -d --privileged --gpus all --network host --cgroupns host -v /sys/fs/cgroup:/sys/fs/cgroup:rw --name ${CONTAINER} $IMAGE /sbin/init
 
-# mount
-docker run -d --mount type=bind,source=/usr/lib/x86_64-linux-gnu,target=/opt/tiger/x86_64-linux-gnu --name <container_name> <image_name> /usr/bin/bash -c 'while true; do sleep 3600; done'
-
-# gpu
-docker run -d --gpus all --name <container_name> <image_name> /usr/bin/bash -c 'while true; do sleep 3600; done'
 
 # enter docker
 docker exec -it <container_name> bash
@@ -21,6 +38,13 @@ systemctl restart docker
 
 # Copy
 docker cp <host_src_path> <container_name_or_id>:<container_dest_path>
+docker cp <container_name_or_id>:<container_src_path> <host_dest_path>
 
 # build
-docker build -t data.aml.snapshot_torch291_cuda:v1.0.0.1 -f Dockerfile.torch .
+docker build -t sony_torch291_cuda:v1.0.0.1 -f Dockerfile.torch .
+
+docker commit sony_pilot_torch_270 sony_pilot_torch_270:latest
+
+docker save -o ${HOME}/workspace/docker_images/sony_pilot_torch_270.tar sony_pilot_torch_270:latest
+
+docker load -i sony_pilot_torch_270.tar
