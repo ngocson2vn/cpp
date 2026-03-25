17d97d2b4a01295bfe445273fcf0f4d33b1ce862
Son Nguyen <son.nguyen@bytedance.com>
2026-03-25 05:56:14 +0000

    Update ADL example

--- Examples/ADL/main.cc
+++ Examples/ADL/main.cc
@@ -3,6 +3,22 @@
 
 #include "hashing.h"
 
+// ADL will ignore this function because namespace `sony` differs `std`
+// which is the namespace of the type of `vec` argument.
+namespace sony {
+  template <typename T> 
+  std::size_t do_hash(const std::vector<T>& vec) {
+    std::cout << "sony::do_hash() is called" << std::endl;
+    auto hash_fn = std::hash<T>();
+    std::size_t ret = 0;
+    for (const auto& e : vec) {
+      ret += hash_fn(e);
+    }
+
+    return ret;
+  }
+}
+
 // main
 //   -> sony::hash_fn(vec)
 //     -> do_hash(vec)
@@ -12,6 +28,7 @@
 namespace std {
   template <typename T> 
   std::size_t do_hash(const std::vector<T>& vec) {
+    std::cout << "std::do_hash() is called" << std::endl;
     auto hash_fn = std::hash<T>();
     std::size_t ret = 0;
     for (const auto& e : vec) {
