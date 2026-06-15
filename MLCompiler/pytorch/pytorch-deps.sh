a5b5ea4bab4b52d656cafbec2aaafd484e9bf144
Son Nguyen <son.nguyen@bytedance.com>
2026-06-15 08:31:14 +0000

    How to find CUDA version

--- MLCompiler/pytorch/pytorch-deps.sh
+++ MLCompiler/pytorch/pytorch-deps.sh
@@ -14,6 +14,10 @@ ln -sf /usr/lib/llvm-14/lib/libomp.so.5 /lib/x86_64-linux-gnu/libomp.so
 # pytorch
 /usr/local/tao/agent/modules/bvc/bin/bvc clone aml/lagrange/pytorch --version 1.0.0.498
 
+# Verify CUDA version used in a pytorch build job
+# Search for 'Found CUDA'
+# Result: Found CUDA: /usr/local/cuda (found version "12.8") 
+
 #====================================
 # Docker image deps
 #====================================
