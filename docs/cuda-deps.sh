807bc7725a27122a5e225ff175f3d8b666b701a3
Son Nguyen <son.nguyen@bytedance.com>
2026-06-17 08:26:37 +0000

    Update cuda deps

--- docs/cuda-deps.sh
+++ docs/cuda-deps.sh
@@ -1,6 +1,9 @@
 # cuda 12.4.1
 wget https://developer.download.nvidia.com/compute/cuda/12.4.1/local_installers/cuda_12.4.1_550.54.15_linux.run
 
+# cuda 12.8
+wget https://developer.download.nvidia.com/compute/cuda/12.8.0/local_installers/cuda_12.8.0_570.86.10_linux.run
+
 # cuda 13.1.0
 wget https://developer.download.nvidia.com/compute/cuda/13.1.0/local_installers/cuda_13.1.0_590.44.01_linux.run
 
