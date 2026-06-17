b086a043db2f75c6dae731be5dbba25a0085360f
Son Nguyen <son.nguyen@bytedance.com>
2026-06-17 08:04:34 +0000

    Add docs/tmux.sh

---
+++ docs/tmux.sh
@@ -0,0 +1,18 @@
+# Create new session
+tmux new -s SESSION_NAME
+
+# Detach
+Ctr + b, then `d`
+
+# List sessions
+tmux ls
+
+# Attach
+tmux a
+tmux a -t SESSION_NAME
+
+# Split pane horizontally
+Ctr + b, then `"`
+
+# Split pane vertically
+Ctr + b, then `%`
