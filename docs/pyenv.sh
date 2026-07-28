63f8d61f4dc958849a05d3857b914e825b3f3349
Son Nguyen <son.nguyen@bytedance.com>
2026-07-28 07:33:37 +0000

    Update

--- docs/pyenv.sh
+++ docs/pyenv.sh
@@ -1,13 +1,13 @@
+sudo apt update; sudo apt install -y build-essential libssl-dev zlib1g-dev \
+libbz2-dev libreadline-dev libsqlite3-dev curl \
+libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
+
 curl https://pyenv.run | bash
 echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
 echo '[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
 echo 'eval "$(pyenv init - bash)"' >> ~/.bashrc
 
 exec bash -l
-
-sudo apt update; sudo apt install -y build-essential libssl-dev zlib1g-dev \
-libbz2-dev libreadline-dev libsqlite3-dev curl \
-libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
 
 pyenv install -kf 3.11.2
 pyenv global 3.11.2
