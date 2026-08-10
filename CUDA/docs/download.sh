fb485b27601c19a0fcc578a7f13570b145357cc7
Son Nguyen <son.nguyen@bytedance.com>
2026-08-10 09:07:24 +0000

    Update

--- CUDA/docs/download.sh
+++ CUDA/docs/download.sh
@@ -1,1 +1,7 @@
 wget https://developer.download.nvidia.com/compute/cuda/13.3.1/local_installers/cuda_13.3.1_610.43.02_linux.run
+
+# x86_64
+wget https://developer.download.nvidia.com/compute/cuda/13.1.0/local_installers/cuda_13.1.0_590.44.01_linux.run
+
+# cuda 13.1.0 arm64
+wget https://developer.download.nvidia.com/compute/cuda/13.1.0/local_installers/cuda_13.1.0_590.44.01_linux_sbsa.run
